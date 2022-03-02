7f9f38f9a2e4d58b6ebf2942b282f422f839b7a9
JuanOssa1 <juan.ossa@hotmail.com>
2022-03-01 22:47:36 -0500

    Hasta clase 10

--- assets/index.js
+++ assets/index.js
@@ -11,7 +11,7 @@ import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({ el: video, plugins: [/*new AutoPlay()*/] });
 /**Aqui se observa */
 
 const buttonMute = document.getElementById('muteunmute');
