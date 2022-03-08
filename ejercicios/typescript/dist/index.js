290065cb0d7b335a75bd20f66117e00a2dc3f891
JuanOssa1 <juan.ossa@hotmail.com>
2022-03-08 17:56:29 -0500

    Typescript bug solved

---
+++ ejercicios/typescript/dist/index.js
@@ -0,0 +1,8 @@
+var message = 'Hello World';
+console.log(message);
+console.log("Hello, TypeScript");
+function add(a, b) {
+    return a + b;
+}
+var sum = add(2, 3);
+console.log(sum);
---
+++ ejercicios/typescript/index.ts
@@ -0,0 +1,10 @@
+var message: string = 'Hello World';
+console.log(message);
+console.log("Hello, TypeScript");
+
+function add(a: number, b: number){
+    return a + b;
+
+}
+const sum = add(2,3)
+console.log(sum)
